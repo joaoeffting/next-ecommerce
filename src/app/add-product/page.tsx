5194245b9426561debf90f52fd448467b9a98dfc
Joao Effting <joaopaulo.effting@kindredgroup.com>
2023-10-14 16:01:10 +0200

    Adding product list page

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,4 +1,4 @@
-import { FormSubmitButton } from "@/components/formSubmitButton";
+import { FormSubmitButton } from "@/components/FormSubmitButton";
 import { prisma } from "@/lib/db/prisma";
 import { redirect } from "next/navigation";
 
